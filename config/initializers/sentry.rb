8dbacd910288ce526cd96fef3346c87fd1f863f5
Maximiliano Ustria <maxiustria@gmail.com>
2022-11-02 19:53:17 -0300

    disabled sentry on dev

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,7 @@
 Sentry.init do |config|
-  config.dsn = 'https://215b5afc2a9f406e9baf1ccea17662b2@o4504057273319424.ingest.sentry.io/4504057320898560'
+  if Rails.env.production?
+    config.dsn = 'https://215b5afc2a9f406e9baf1ccea17662b2@o4504057273319424.ingest.sentry.io/4504057320898560'
+  end
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
