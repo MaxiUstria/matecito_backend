b033f4bb0a1ad9bb2e7c8201e8e7f3ce3183abf2
Maximiliano Ustria <maxiustria@gmail.com>
2022-11-08 00:04:19 -0300

    fix

--- app/jobs/increase_objective_counter_job.rb
+++ app/jobs/increase_objective_counter_job.rb
@@ -5,7 +5,7 @@ class IncreaseObjectiveCounterJob < ApplicationJob
     user.objectives.where(state: %w[active goal_reached]).where(
       'end_date >= ? OR end_date is NULL', donation.created_at.to_date
     ).each do |objective|
-      increment_current_amount(objective, donation)
+      increment_current_amount(objective, donation, objective.current_amount)
       rescue ActiveRecord::RecordInvalid => e
         Sentry.capture_exception(e)
         next
@@ -14,8 +14,8 @@ class IncreaseObjectiveCounterJob < ApplicationJob
 
   private
 
-  def increment_current_amount(objective, donation)
-    objective.update!(current_amount: objective.current_amount + donation.amount)
+  def increment_current_amount(objective, donation, current_amount)
+    objective.update!(current_amount: current_amount + donation.amount)
     objective.reached! if objective.current_amount >= objective.target_amount && objective.active?
   end
 end
